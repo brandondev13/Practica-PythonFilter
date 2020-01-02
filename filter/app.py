4f5298783e15a6607bfd2dbc0e9ed2f5cab41b56
DESKTOP-6P90DSI\Brandon Mejia <brandonmejiarestrepo@gmail.com>
2020-01-01 19:53:43 -0500

    commit

--- filter/app.py
+++ filter/app.py
@@ -3,7 +3,7 @@ from random import randint
 from datetime import datetime, timedelta
 
 # Fijar la fecha de inicio
-start_date = datetime(2020, 12, 1)
+start_date = datetime(2019, 12, 31)
 
 # Iterar sobre los días del año
 for i in range(1, 50):
