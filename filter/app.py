c035e2c0eed04b37f1e38d5823c353d10a96eb49
DESKTOP-6P90DSI\Brandon Mejia <brandonmejiarestrepo@gmail.com>
2023-01-02 20:47:12 -0500

    commit

--- filter/app.py
+++ filter/app.py
@@ -3,10 +3,10 @@ from random import randint
 from datetime import datetime, timedelta
 
 # Fijar la fecha de inicio
-start_date = datetime(2022, 10, 1)
+start_date = datetime(2023, 1, 1)
 
 # Iterar sobre los días del año
-for i in range(1, 100):
+for i in range(1, 16):
     # Generar un número aleatorio de commits para el día actual
     for j in range(0, randint(1, 20)):
         # Calcular la fecha para el commit
